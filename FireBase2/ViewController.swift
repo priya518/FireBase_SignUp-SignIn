6d4a660224f4efd098f68e0e442a7a665821149a
Priya Patel <pp520942@gmail.com>
2019-12-27 18:44:23 +0530

    Initial Commit

---
+++ FireBase2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FireBase2
+//
+//  Created by Priya Patel on 27/12/19.
+//  Copyright © 2019 priya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
